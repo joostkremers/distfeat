86f8bb59895c09ee657e47fe041b153dceba3e55
Joost Kremers <joostkremers@fastmail.fm>
2018-08-19 20:36:10 +0200

    Add `describe-phoneme`.

--- src/distfeat/core.clj
+++ src/distfeat/core.clj
@@ -1,5 +1,6 @@
 (ns distfeat.core
   (:require [clojure.set :as set]
+            [clojure.string :as string]
             [distfeat.phonemes :as ph])
   (:gen-class))
 
@@ -21,10 +22,25 @@
                         (set features)
                         features))))
 
+(defn feature->string
+  "Turn `feature` into a displayable string.
+  `feature` should be a two-element vector of [feature-name value]. `conversion`
+  is a hash map describing how the values `true` and `false` should be
+  represented. Default is \"+\" for `true` and \"-\" for `false'."
+  ([feature] (feature->string {true "+" false "-"} feature))
+  ([conversion feature]
+   (if (ph/place-feature? (first feature))
+     (if (second feature) (name (first feature)) "") ; Filter out place features with value `false`.
+     (str (get conversion (second feature)) (name (first feature))))))
+
 (defn describe-phoneme
-  "Return a string describing `phoneme`."
-  [phoneme]
-  )
+  "Return a string describing `features`.
+  `sep` is the separator, `conv` a hash map describing how the values `true` and
+  `false` should be represented. Default is \"+\" for `true` and \"-\" for
+  `false'."
+  [features & {:keys [sep conv]}]
+  (let [convertors (merge {true "+" false "-"} conv)]
+    (string/join (or sep " ") (map #(feature->string convertors %) features))))
 
 (defn -main
   "I don't do a whole lot ... yet."
